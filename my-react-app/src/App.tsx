5ab087d7259ac27d6e7092b2d6120ec07c7114ca
Qiu-Nick <nicolas.theaux.peria@gmail.com>
2024-10-23 17:42:45 +0200

    IL EST VIVANT! ENCORE

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -2,12 +2,22 @@ import React from "react";
 import PokemonCard from "./components/PokemonCard";
 
 function App() {
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+      imgSrc: "",
+    },
+  ];
+
   return (
-   <div>
-      <PokemonCard>
-        
-      </PokemonCard>
-   </div>
+    <div>
+      <PokemonCard pokemon={pokemonList[0]} />
+    </div>
   );
 }
 
--- my-react-app/src/components/PokemonCard.tsx
+++ my-react-app/src/components/PokemonCard.tsx
@@ -1,38 +1,17 @@
 import React from "react";
 
-const PokemonCard = () => {
-    
-    const pokemonList = [
-        {
-            name: "bulbasaur",
-            imgSrc:
-            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-        },
-        {
-            name: "mew",
-            imgSrc:
-            "",
-        },
-    ];
-    
-    const pokemon = pokemonList[1];
+function PokemonCard({ pokemon }) {
+  return (
+    <figure>
+      {pokemon.imgSrc ? (
+        <img src={pokemon.imgSrc} alt={pokemon.name} />
+      ) : (
+        <p>???</p>
+      )}
 
-    return (
-      <figure>
-        {
-            pokemon.imgSrc
-        ? <img
-          src={pokemon.imgSrc}
-          alt={pokemon.name}
-          />
-        : <p>???</p>
-        }
-
-        <figcaption>
-            {pokemon.name} 
-        </figcaption>
-      </figure>
-    );
-  };
+      <figcaption>{pokemon.name}</figcaption>
+    </figure>
+  );
+}
 
 export default PokemonCard;
